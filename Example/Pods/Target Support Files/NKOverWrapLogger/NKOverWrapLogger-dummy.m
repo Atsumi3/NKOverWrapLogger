d4a2e853648769daba4472b951284eb165bfe752
atsumi <atsumi@mail.bizen.jp>
2018-09-27 02:52:32 +0900

    Initial Commit

---
+++ Example/Pods/Target Support Files/NKOverWrapLogger/NKOverWrapLogger-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NKOverWrapLogger : NSObject
+@end
+@implementation PodsDummy_NKOverWrapLogger
+@end
---
+++ Example/Pods/Target Support Files/Pods-NKOverWrapLogger_Example/Pods-NKOverWrapLogger_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NKOverWrapLogger_Example : NSObject
+@end
+@implementation PodsDummy_Pods_NKOverWrapLogger_Example
+@end
